f2e15bdfcf4d8411194a605b1711010e91c0ea80
TolgaV <tolga.varol@outlook.com>
2017-09-18 01:21:13 +0200

    Monitor fixed.

--- sc-programs/adder-pure-cpp/monitor.cpp
+++ sc-programs/adder-pure-cpp/monitor.cpp
@@ -2,13 +2,14 @@
 #include "monitor.h"
 
 void Monitor::watcher() {
-	cout << " At " << sc_simulation_time() << " input is : ";
-	cout << a << " , " << b << " output is : " << c << endl;
+	std::cout << " At " << sc_simulation_time() << " input is : ";
+	std::cout << a << " , " << b << " output is : " << c << endl;
 }
 
-
+/* Monitor only has to be sensitive to output in order for not to
+print duplicate stuff everytime something changes */
 Monitor::Monitor(sc_module_name _name)
 	:sc_module(_name) {
 	SC_METHOD(watcher);
-	sensitive << a, b, c;
+	sensitive << c;//a, b, c, clock.pos();
 }
