e0744bb0647f745bba304128460203f6d11bd210
TolgaV <tolga.varol@outlook.com>
2017-10-04 02:42:36 +0200

    Comments and Explanation Added.

--- sc-labs/lab-1-3/testbench.cpp
+++ sc-labs/lab-1-3/testbench.cpp
@@ -9,8 +9,21 @@ void driver::prc_drive() {
 		for (i = 0; i<26; i++) {
 			d_char1.write(arr[i]);
 			d_char2.write(arr[i]);
+			
+			/* Explanation of the behavior */
+			//Since we do not have any time related statement after commenting wait out
+			//SystemC is not advancing the time, it even isn't advancing delta cycle.
+			//Instead immediate notification occurs during evaluate update paradox here.
+			//See also "SystemC Simulation Cycle"
+			//cout << i;
+			cout << "Simulation time is: " << sc_simulation_time<< "\n";
+			cout << "Delta Cycle count is: " << sc_delta_count << "\n";
+			
+			//Required to advance time.
 			wait(25, SC_NS);
-			for (int i = 0; i<100000000; i++);// dealy in order to see the output.
+
+			// delay in order to see the output.
+			for (int i = 0; i < 100000000; i++);// { cout << "xxx"; };
 		}
 
 	}
