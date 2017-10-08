8b41944eac03ca53990f9fd27ca8748497efee3a
TolgaV <tolga.varol@outlook.com>
2017-10-08 11:42:01 +0200

    Initialization interface defined

--- sc-labs/lab-2-1/ram.cpp
+++ sc-labs/lab-2-1/ram.cpp
@@ -3,43 +3,97 @@
 
 
 void RandomAccessMemory::loadInitialize(){
-	readFile();
+	if (initFlag) {
+		cout << "\n\nYEEEES\n\n";
+		readFile();
+	}
+	else {
+		cout << "\n\nNOOOOOO\n\n";
+		//fileIsRead();
+	}
 }
 
 void RandomAccessMemory::readFile() {
 	/* Open the File */
 	ifstream inFile;
-	inFile.open("T1.txt");							//We can directly refer the file without absolute path since its in Resource Files.
+	inFile.open("Resources/T1.txt");							//We can directly refer the file without absolute path since its in Resource Files.
 	/* Check for an Error */
-	if (inFile.fail()) {
-		cerr << "Error Opening File\n";
-		cout << "Check Stderr\n";
+	if (!inFile) {
+		cout << "\nUnable to Load File...\n\n";
 		exit(1);
 	}
-	std::string line;
-	
+
 	int i = 0;
+	bool eofFlag = false;
+	std::string line;
+	std::string subline;
+
+	cout << "ADDR\tV\tDATA\t\n";
+	cout << "--------------------------\n";
 	/* Loop reading until EOF or bad input */
-	while (!inFile.eof()) {
-		inFile >> memory[i];
-		i++;
-		if (i < memorySize) {
+	while (!inFile.bad() && (i < memorySize)){
+		std::getline(inFile, line);
+		if (inFile.eof()) {
+			eofFlag = true;
+			cout << "MOUHAHAHAHAHA - EOOOOOOF\n";
+			if (eofFlag)
+				fileIsRead(i);
+		}
+		/* Body of Function to be Disabled after Calling fileIsRead() */
+		if (!eofFlag) {
+			int j = line.length();		
+			//cout << j << "\t";	//For out of range exception
+			if ((j < 8) && (j > 3)) {
+				subline = line.substr((line.length() - 2), line.length());
+			}
+
+			/* Initializing RAM */
+			ra->p_dataAddress[i] = subline;
+			ra->ram_address_valid[i] = true;
+			ra->ram_address[i] = i;
+			cout << *(ra->ram_address + i) << "\t" << *(ra->ram_address_valid + i) \
+				<< "\t" << *(ra->p_dataAddress + i) << "\t" << sizeof(subline) << "\t\n";
+
+			i++;
+		}
+		/*if (i < memorySize) {
 			continue;
 		}
 		else {
-			cerr << "Address Out of Range\n";
+			cerr << "\nAddress Out of Range\n";
 			cout << "Check Stderr\n";
 			SC_REPORT_ERROR("RAM", "Addresses that are out of range are not loaded!\n");
 			break;
-		}
+		}*/
 	}
 	inFile.close();
+	initFlag = 0;
+}
+
+void RandomAccessMemory::fileIsRead(int &i) {
+	std::string unavailable = "XX";
+	while (i < memorySize) {
+		ra->p_dataAddress[i] = unavailable;
+		ra->ram_address_valid[i] = false;
+		ra->ram_address[i] = i;
+		
+		cout << *(ra->ram_address + i) << "\t" << *(ra->ram_address_valid + i) \
+			<< "\t" << *(ra->p_dataAddress + i) << "\n";// << "\t" << sizeof(subline) << "\t\n";
+		i++;
+	}
+	cout << "OOOOOOOOOOOOOOOOOOOOOOOOOOOOO";
 }
 
 void RandomAccessMemory::read(){
-
+/*	while (true) {
+		
+		en false;
+	
+	adsdsadasds
+		if(this->en)
+	}*/
 }
 
 void RandomAccessMemory::write() {
-
+	
 }
