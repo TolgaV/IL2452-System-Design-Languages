6eb24ce540ca9f62bdbc3232261335e8e5ad4b6c
TolgaV <tolga.varol@outlook.com>
2017-10-08 11:42:41 +0200

    RAM Class created

--- sc-labs/lab-2-1/ram.h
+++ sc-labs/lab-2-1/ram.h
@@ -2,17 +2,34 @@
 #include "stdafx.h"
 #include <fstream> //Stream class to read from filesm, input file stream variable is called "ifstream"
 #include <string>
-//Maybe use a struct to keep address thingy.sfgdfzxnhgc,'j;lghmh;kcfnb;kxfthnb.x,knb
 class RamAddress {
 public:
+	std::string *p_dataAddress;
+	//char *p_dataAddress;
+	//std::string data[2];
 	int *ram_address;
-	char *ram_data;
+	bool *ram_address_valid;
+	//char *ram_data[2];
+	
+	
 
-	RamAddress(int size) :ram_memory_size(size) {
+	RamAddress(int mem_size) 
+		:ram_memory_size(mem_size) {
+		cout << " ram memory size is \n\n" << ram_memory_size;
+		
+		p_dataAddress = new std::string[ram_memory_size];
+		ram_address_valid = new bool[ram_memory_size];
 		ram_address = new int[ram_memory_size];
-		ram_data = new char[ram_memory_size];
+		
+		/*ram_address_used = new bool[ram_memory_size];
+
+		for (int i = 0; i < ram_memory_size; i++) {
+			ram_address_used[i] = false;
+			p_dataAddress[i] = &p_data[i];
+		}*/
+		cout << "\nRamAddress Object Created.\n";
 	}
-private:
+//private:
 	int ram_memory_size;
 };
 
@@ -23,38 +40,37 @@ public:
 	sc_in<bool> en{ "CE" };						//Boolean input enable port
 	sc_in<bool> mode{ "WE" };					//Boolean input mode (R/W) select port 
 
-	char *memory;								//Pointer to memory
-	RamAddress *ra;				///////////////////////////////////////////////////////////////////CONTINUE FROM HERE, GOT TO INSTANTIATE THIS IN THE CONSTRUCTOR AND FIND A WAY TO READ FILE INTO ITS MEMBERS USING loadInitiaize();
+	RamAddress *ra;
 
 	void read();
 	void write();
 
 	//Initialize the constructor as RandomAccessMemory(name, size)
-	RandomAccessMemory(sc_module_name name, int size) :sc_module(name), memorySize(size) {
-		memory = new char[memorySize];
-
+	RandomAccessMemory(sc_module_name name, int memory_size, bool debug)
+		:sc_module(name), memorySize(memory_size), debugFlag(debug) {
+		
+		ra = new RamAddress(memory_size);
+		initFlag = true;
 		cout << "\n\n\nLoading initial data... \n";
-		loadInitialize();
+		loadInitialize();						//First call for filling memory array
 
-		int *address = new int[memorySize];		//Array to keep address values
-		ra = new RamAddress(size);
-		ra->ram_address = somechar here, this assignment to be done via the function;
+		SC_THREAD(read);
+		sensitive << en, mode, data;
+		SC_THREAD(write);
+		sensitive << en, mode, address;
 	}
-	
-	~RandomAccessMemory() {
-		delete [] memory;
-	}
+	SC_HAS_PROCESS(RandomAccessMemory);			//Registering methods as SysC processes.
 
 private:
 	/* Private Member Functions */
 	void loadInitialize();
 	void readFile();
-
+	void fileIsRead(int&);
 	/* Private Data Members */
 	bool enable;
-	bool mode;
-	//sc_int<32> address;
-	//sc_int<8> data;
+	bool debugFlag;
+	bool initFlag;
+	//bool mode;
 	
 	//Data member for keeping memory size
 	int memorySize;
